50acb8c5bcbf9928fa6c1c615e476acdb26458fc
Cesar Willy Mamani Canaza <48421004+cesarwillymc@users.noreply.github.com>
2022-04-28 20:17:22 -0500

    Configuration manifest with Application

---
+++ app/src/main/java/com/cesarwillymc/technicaltestwordbox/App.kt
@@ -0,0 +1,13 @@
+package com.cesarwillymc.technicaltestwordbox
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by cesarwillymamanicanaza on 28/04/22.
+ * cesarwilly.mc@gmail.com
+ *
+ * Lima, Peru.
+ */
+@HiltAndroidApp
+open class App : Application()
--- app/src/main/java/com/cesarwillymc/technicaltestwordbox/ui/MainActivity.kt
+++ app/src/main/java/com/cesarwillymc/technicaltestwordbox/ui/MainActivity.kt
@@ -1,10 +1,16 @@
-package com.cesarwillymc.technicaltestwordbox
+package com.cesarwillymc.technicaltestwordbox.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.cesarwillymc.technicaltestwordbox.ui.theme.GreenCrossTheme
 
+/**
+ * Created by cesarwillymamanicanaza on 28/04/22.
+ * cesarwilly.mc@gmail.com
+ *
+ * Lima, Peru.
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
